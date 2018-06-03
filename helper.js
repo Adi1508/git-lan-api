6a3ff66c62756a064db4bbac2d19c69b34de5597
Aditya Kumar <adityak007mr@gmail.com>
2018-06-03 17:27:11 +0530

    async issue

--- helper.js
+++ helper.js
@@ -1,100 +1,178 @@
 var helpers = {};
-var https   = require('https');
+var https = require('https');
+var async = require("async");
+var fiber = require("fibers");
+//var EventEmitter = require("events").EventEmitter;
+//var repData = new EventEmitter();
 
-helpers.getRepos = (username)=>{
-  return new Promise( (resolve, reject) => {
-    var repos = [];
+helpers.getRepos = (username) => {
+    return new Promise((resolve, reject) => {
+        var repos = [];
 
-    var optionGit = {
-        'protocol' : 'https:',
-        'hostname': 'api.github.com',
-        'path': '/users/'+username+'/repos',
-        'method': 'GET',
-        'headers': {'user-agent' : 'node.js'}
-    };
-
-    var request = https.request(optionGit,(response)=>{
-       
- 
-           var body = ''; 
-          response.on('data', function(chunk){
-              body+=chunk.toString('utf8');
-          });
-        
-         response.on('end', ()=>{
-            var obj = JSON.parse(body.toString());
-
-            var keys = Object.keys(obj);
-            for(var i =0; i<keys.length; i++){
-                var key = keys[i];
-                var value = obj[key];
-
-                var subkey = Object.keys(value);
-                for(var k = 0; k< subkey.length; k++){
-                    var subValues = value['name'];
-                }
-                repos.push(subValues);
+        var optionGit = {
+            'protocol': 'https:',
+            'hostname': 'api.github.com',
+            'path': '/users/' + username + '/repos',
+            'method': 'GET',
+            'headers': {
+                'user-agent': 'node.js',
+                'Authorization': 'token 48284cb2a28e434813ae926bfdedd9f8236aa692'
             }
-            resolve(repos);
-        });
-   
-    }); 
-
-     // Bind to the error event so it doesn't get thrown
-        request.on('error',function(e){
-          reject(e);
-        });
-
-      request.end();
-   });
-
- }
-
-
-helpers.getInfo = (repo) => {
-    return new Promise( (resolve, reject) => {
-        var optionGitLanguage = {
-            host: 'api.github.com',
-            path: '/repos/'+ userName + '/' + repos[g] + '/languages',
-            method: 'GET',
-            headers: {'user-agent': 'node.js'}
         };
 
-        //console.log('path2: '+optionGitLanguage.path);
+        //console.log(optionGit.path);
 
-        var o = {};
-        var key = 'list of languages in your public repos';
-        o[key]=[];
-        var finalArray = [];
+        var request = https.request(optionGit, (response) => {
 
-        var request2 = https.request(optionGitLanguage, function(response){
-            console.log('inside request 2');
-            var body2 = '';
-            response.on('data', function(chunk){
-                body2+=chunk.toString('utf8');
+            var body = '';
+            response.on('data', function (chunk) {
+                body += chunk.toString('utf8');
             });
-            response.on('end', function(){
-                var obj2 = JSON.parse(body2.toString());
 
-                var keys2 = Object.keys(obj2);
-                for(var h = 0; h<keys2.length; h++){
-                    var key2 = keys2[h];
-                    var value2 = obj2[key2];
-                    console.log(h+' '+key2);
-                    var data = {
-                        repo: repos[g],
-                        languages: key2
+            var finalArray = [];
+
+            response.on('end', () => {
+                var obj = JSON.parse(body.toString());
+
+                var keys = Object.keys(obj);
+                for (var i = 0; i < keys.length; i++) {
+                    var key = keys[i];
+                    var value = obj[key];
+
+                    var subkey = Object.keys(value);
+                    for (var k = 0; k < subkey.length; k++) {
+                        var subValues = value['name'];
                     }
+                    repos.push(subValues);
                 }
-                o[key].push(data);
+                var reps = JSON.stringify(repos);
+                var reposArray = JSON.parse(reps);
+
+                //console.log(reposArray);
+
+                for (var i = 0; i < reposArray.length; i++) {
+
+                    var optionGitLanguage = {
+                        host: 'api.github.com',
+                        path: '/repos/' + username + '/' + reposArray[i] + '/languages',
+                        method: 'GET',
+                        headers: {
+                            'user-agent': 'node.js',
+                            'Authorization': 'token 48284cb2a28e434813ae926bfdedd9f8236aa692'
+                        }
+                    };
+
+                    var o = {};
+                    var key = 'lan';
+                    o[key] = [];
+
+                    var request2 = https.request(optionGitLanguage, function (response) {
+
+                        var body2 = '';
+                        response.on('data', function (chunk) {
+                            body2 += chunk.toString('utf8');
+                        });
+                        response.on('end', function () {
+                            o[key].push(JSON.stringify(body2));
+                            //console.log(JSON.stringify(o));
+                            //repData = JSON.stringify(o);
+                            //repData.emit('update');
+                        });
+                        //console.log(JSON.stringify(o));
+                        //resolve(JSON.stringify(o));
+                    });
+                    request2.end();
+                }
+                //console.log(o.length);
                 console.log(JSON.stringify(o));
+                /*repData.on('update', function () {
+                    console.log(repData);
+                })*/
+                //resolve(repos);
+
             });
+
         });
 
+        // Bind to the error event so it doesn't get thrown
+        request.on('error', function (e) {
+            reject(e);
+        });
 
-        request2.end();
-        resolve(o);
+        request.end();
     });
+
 }
+
+
+
+/*helpers.funCall = (reps, username) => {
+    var arr = [];
+
+    return new Promise((resolve, reject) => {
+        var repos = JSON.stringify(reps);
+        var reposArray = JSON.parse(repos);
+
+        for (var i = 0; i < reposArray.length; i++) {
+
+            helpers.getInfo(reposArray[i], username).then((data) => {
+                var resp = JSON.stringify(data);
+                arr.push(resp);
+            });
+
+
+        }
+        console.log(datares);
+        console.log('arr: ' + arr);
+    });
+
+}
+
+helpers.getInfo = (reps, userName) => {
+    return new Promise((resolve, reject) => {
+
+
+        var o = {};
+
+        var repos = JSON.stringify(reps);
+        var reposArray = JSON.parse(repos);
+
+        for (var i = 0; i < reposArray.length; i++) {
+
+            var optionGitLanguage = {
+                host: 'api.github.com',
+                path: '/repos/' + userName + '/' + reposArray[i] + '/languages',
+                method: 'GET',
+                headers: {
+                    'user-agent': 'node.js',
+                    'Authorization': 'token 48284cb2a28e434813ae926bfdedd9f8236aa692'
+                }
+            };
+
+
+            var key = 'lan';
+            o[key] = [];
+
+            var request2 = https.request(optionGitLanguage, function (response) {
+
+                var body2 = '';
+                response.on('data', function (chunk) {
+                    body2 += chunk.toString('utf8');
+                });
+                response.on('end', function () {
+                    o[key].push(JSON.stringify(body2));
+
+                });
+                //console.log(JSON.stringify(o));
+                //resolve(o);
+            });
+            request2.end();
+        }
+
+        console.log(JSON.stringify(o));
+
+
+    });
+}*/
 
 module.exports = helpers;
--- index.js
+++ index.js
@@ -1,40 +1,46 @@
 var express = require('express');
-var app  = express();
+var app = express();
 var bodyParser = require('body-parser');
-var https=require('https');
+var https = require('https');
 var Promise = require('promise');
 var helpers = require('./helper');
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8383;
 
 var router = express.Router();
 
-router.use(function(req, res, next){
+router.use(function (req, res, next) {
     console.log('api testing..');
     next();
 });
 
-var count=0;
+var count = 0;
 
 router.route('/api/:username')
-    .get(function(req, res){
+    .get(function (req, res) {
         var userName = req.params.username;
         console.log(userName);
-           
-        /*var reposObject = */helpers.getRepos(userName).then((reposObject)=>{
-            console.log("repos object: "+reposObject);
-            res.json(reposObject);
+        var infoGet;
+        helpers.getRepos(userName).then((reposObject)=>{
+            infoGet = JSON.stringify(reposObject);
+            console.log("infoGet : "+ infoGet); 
+            res.json(JSON.parse(infoGet));
         }).catch((err)=>{
-            res.json({"reponse":false});
+            res.json(err);
         });
+        //console.log('output after helper: '+JSON.parse(infoGet));
     });
 
 router.route('/api')
-    .get(function(req, res){
-        res.json({message: 'Welcome to git-lan-api, to use the API please pass your username in the url with the API'})
+    .get(function (req, res) {
+        res.json({
+            message: 'Welcome to git-lan-api, to use the API please pass your username in the url with the API'
+        })
     });
 
 //register the routes
@@ -42,4 +48,4 @@ app.use('/v1', router);
 
 //starting the server
 app.listen(port);
-console.log('Server running at Port '+port);
+console.log('Server running at Port ' + port);
