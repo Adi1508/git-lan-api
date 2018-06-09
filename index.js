4847c0989c3e91c63d6346791a86ec42d18f9f19
Aditya Kumar <adityak007mr@gmail.com>
2018-06-10 01:11:53 +0530

    updates

--- index.js
+++ index.js
@@ -21,71 +21,117 @@ router.use(function (req, res, next) {
     next();
 });
 
-router.route('/home').get((req, res)=>{
-	res.sendFile(pathView + 'index.html');
+router.route('/home').get((req, res) => {
+    res.sendFile(pathView + 'index.html');
 });
 
-router.get('/v1/api',function (req, res) {
-        var userName = req.query.param1;
-        var authToken = req.query.param2;
-        helpers.getRepos(userName, authToken).then((reposObject) => {
+router.get('/v1/api/noauth', (req, res) => {
+    var userName = req.query.param1;
+    helpers.getReposNoAuth(userName).then((reposObject) => {
 
-            return reposObject;
+        return reposObject;
 
-        }).then((obj) => {
-            var reps = JSON.stringify(obj);
-            var reposArray = JSON.parse(reps);
+    }).then((obj) => {
+        var reps = JSON.stringify(obj);
+        var reposArray = JSON.parse(reps);
 
-            var urls = [];
-            for (var i = 0; i < reposArray.length; i++) {
+        var urls = [];
+        for (var i = 0; i < reposArray.length; i++) {
 
-                var optionGitLanguage = {
-                    host: 'api.github.com',
-                    path: '/repos/' + userName + '/' + reposArray[i] + '/languages',
-                    method: 'GET',
-                    headers: {
-                        'user-agent': 'node.js',
-                        'Authorization': 'token '+authToken //insert the github auth access token
-                    }
-                };
+            var optionGitLanguage = {
+                host: 'api.github.com',
+                path: '/repos/' + userName + '/' + reposArray[i] + '/languages',
+                method: 'GET',
+                headers: {
+                    'user-agent': 'node.js'
+                }
+            };
 
-                urls.push(optionGitLanguage);
-            }
+            urls.push(optionGitLanguage);
+        }
 
-            var responses = [];
-            var completed_requests = 0;
+        var responses = [];
+        var completed_requests = 0;
 
-            for (var a = 0; a < urls.length; a++) {
+        for (var a = 0; a < urls.length; a++) {
 
-                var request2 = https.request(urls[a], function (response) {
+            var request2 = https.request(urls[a], function (response) {
 
-                    var body2 = '';
-                    response.on('data', function (chunk) {
-                        body2 += chunk.toString('utf8');
-                    });
-                    response.on('end', function () {
-                        responses.push(JSON.parse(body2));
-                        completed_requests++;
-                        if (completed_requests == urls.length) {
-                            // All download done, process responses array
-                            res.json(responses);
-                        }
-                    });
+                var body2 = '';
+                response.on('data', function (chunk) {
+                    body2 += chunk.toString('utf8');
                 });
-                request2.end();
-            }
+                response.on('end', function () {
+                    responses.push(JSON.parse(body2));
+                    completed_requests++;
+                    if (completed_requests == urls.length) {
+                        // All download done, process responses array
+                        res.json(responses);
+                    }
+                });
+            });
+            request2.end();
+        }
 
-        }).catch((err) => {
-            res.json(err);
-        });
+    }).catch((err) => {
+        res.json(err);
     });
+})
 
-/*router.route('/v')
-    .get(function (req, res) {
-        res.json({
-            message: 'Welcome to git-lan-api, to use the API please pass your username in the url with the API'
-        })
-    });*/
+router.get('/v1/api', function (req, res) {
+    var userName = req.query.param1;
+    var authToken = req.query.param2;
+    helpers.getRepos(userName, authToken).then((reposObject) => {
+
+        return reposObject;
+
+    }).then((obj) => {
+        var reps = JSON.stringify(obj);
+        var reposArray = JSON.parse(reps);
+
+        var urls = [];
+        for (var i = 0; i < reposArray.length; i++) {
+
+            var optionGitLanguage = {
+                host: 'api.github.com',
+                path: '/repos/' + userName + '/' + reposArray[i] + '/languages',
+                method: 'GET',
+                headers: {
+                    'user-agent': 'node.js',
+                    'Authorization': 'token ' + authToken //insert the github auth access token
+                }
+            };
+
+            urls.push(optionGitLanguage);
+        }
+
+        var responses = [];
+        var completed_requests = 0;
+
+        for (var a = 0; a < urls.length; a++) {
+
+            var request2 = https.request(urls[a], function (response) {
+
+                var body2 = '';
+                response.on('data', function (chunk) {
+                    body2 += chunk.toString('utf8');
+                });
+                response.on('end', function () {
+                    responses.push(JSON.parse(body2));
+                    completed_requests++;
+                    if (completed_requests == urls.length) {
+                        // All download done, process responses array
+                        res.json(responses);
+                    }
+                });
+            });
+            request2.end();
+        }
+
+    }).catch((err) => {
+        res.json(err);
+    });
+});
 
 //register the routes
 app.use('/', router);
--- public/main.js
+++ public/main.js
@@ -2,57 +2,113 @@ $('#fetch').click(() => {
     var username = document.getElementById("username").value;
     var token = document.getElementById("token").value;
 
-    var data = {};
-    data.param1 = username;
-    data.param2 = token;
+    if (token === null || undefined === token || token === '') {
 
-    $.ajax({
-        type: 'GET',
-        data: data,
-        contentType: 'application/json',
-        url: 'http://localhost:8383/v1/api',
-        dataType: 'json',
-        success: (result) => {
-            var returnMovies = JSON.stringify(result);
-            var items2 = [];
-            var jsonData = JSON.parse(returnMovies);
-            var finalData = [];
-            for (var i = 0; i < jsonData.length; i++) {
-                var counter = Object.keys(jsonData[i]);
-                if (counter.length === 1 && undefined !== counter) {
-                    finalData.push(counter[0]);
-                } else if (counter.length > 1 && undefined !== counter) {
-                    for (var k = 0; k < counter.length; k++) {
-                        finalData.push(counter[k]);
+        var data = {};
+        data.param1 = username;
+
+        $.ajax({
+            type: 'GET',
+            data: data,
+            contentType: 'application/json',
+            url: 'http://localhost:8383/v1/api/noauth',
+            dataType: 'json',
+            success: (result) => {
+                $("#result").empty();
+                var returnMovies = JSON.stringify(result);
+                var items2 = [];
+                var jsonData = JSON.parse(returnMovies);
+                var finalData = [];
+                for (var i = 0; i < jsonData.length; i++) {
+                    var counter = Object.keys(jsonData[i]);
+                    if (counter.length === 1 && undefined !== counter) {
+                        finalData.push(counter[0]);
+                    } else if (counter.length > 1 && undefined !== counter) {
+                        for (var k = 0; k < counter.length; k++) {
+                            finalData.push(counter[k]);
+                        }
                     }
                 }
-            }
-            var lan = [],
-                count = [],
-                prev;
+                var lan = [],
+                    count = [],
+                    prev;
 
-            finalData.sort();
-            for (var i = 0; i < finalData.length; i++) {
-                if (finalData[i] !== prev) {
-                    lan.push(finalData[i]);
-                    count.push(1);
-                } else {
-                    count[count.length - 1]++;
+                finalData.sort();
+                for (var i = 0; i < finalData.length; i++) {
+                    if (finalData[i] !== prev) {
+                        lan.push(finalData[i]);
+                        count.push(1);
+                    } else {
+                        count[count.length - 1]++;
+                    }
+                    prev = finalData[i];
                 }
-                prev = finalData[i];
-            }
 
-            var countSum=0;
-            for(var h =0; h<count.length; h++){
-                countSum = countSum+count[h];
+                var countSum = 0;
+                for (var h = 0; h < count.length; h++) {
+                    countSum = countSum + count[h];
+                }
+                console.log(countSum);
+                for (var g = 0; g < lan.length; g++) {
+                    items2.push('<li class="list-group-item">' + lan[g] + '<span class="badge">' + Math.round((count[g] / countSum) * 100) + ' % </span></li>');
+                }
+                $('#result').append(items2.join(''));
             }
-            console.log(countSum);
-            for(var g =0; g<lan.length; g++){
-                items2.push('<li class="list-group-item">' + lan[g] + '<span class="badge">'+ ((count[g]/countSum)*100) +'% </span></li>');
+        });
+
+    } else {
+        var data = {};
+        data.param1 = username;
+        data.param2 = token;
+
+        $.ajax({
+            type: 'GET',
+            data: data,
+            contentType: 'application/json',
+            url: 'http://localhost:8383/v1/api',
+            dataType: 'json',
+            success: (result) => {
+                $("#result").empty();
+                var returnMovies = JSON.stringify(result);
+                var items2 = [];
+                var jsonData = JSON.parse(returnMovies);
+                var finalData = [];
+                for (var i = 0; i < jsonData.length; i++) {
+                    var counter = Object.keys(jsonData[i]);
+                    if (counter.length === 1 && undefined !== counter) {
+                        finalData.push(counter[0]);
+                    } else if (counter.length > 1 && undefined !== counter) {
+                        for (var k = 0; k < counter.length; k++) {
+                            finalData.push(counter[k]);
+                        }
+                    }
+                }
+                var lan = [],
+                    count = [],
+                    prev;
+
+                finalData.sort();
+                for (var i = 0; i < finalData.length; i++) {
+                    if (finalData[i] !== prev) {
+                        lan.push(finalData[i]);
+                        count.push(1);
+                    } else {
+                        count[count.length - 1]++;
+                    }
+                    prev = finalData[i];
+                }
+
+                var countSum = 0;
+                for (var h = 0; h < count.length; h++) {
+                    countSum = countSum + count[h];
+                }
+                console.log(countSum);
+                for (var g = 0; g < lan.length; g++) {
+                    items2.push('<li class="list-group-item">' + lan[g] + '<span class="badge">' + Math.round((count[g] / countSum) * 100) + ' % </span></li>');
+                }
+                $('#result').append(items2.join(''));
             }
-            $('#result').append( items2.join('') );
-        }
+        });
+    }
 
-
-    });
 });
